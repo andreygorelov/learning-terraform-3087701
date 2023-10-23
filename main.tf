2e07e1e9a12782e70a64f782d66a9acc205cc6d4
Andrey <agorelov@hotmail.com>
2023-10-23 19:41:05 -0400

    Autoscaling fixes

--- main.tf
+++ main.tf
@@ -47,12 +47,6 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
     }
   ]
 
@@ -79,7 +73,7 @@ module "autoscaling" {
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns = module.blog_alb.target_group_arns
-  security_group = [module.blog_sg.security_group_id]
+  security_groups = [module.blog_sg.security_group_id]
 
   image_id = data.aws.app_ami.id
   instance_type = var.instance_type
